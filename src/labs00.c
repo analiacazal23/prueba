d33508c0e93409f740f3fa7a3ec092ac0d09318c
lp1-2021 <analiacazal@gmail.com>
2022-03-04 12:02:48 -0300

    pruebs

---
+++ src/labs00.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : labs00.c
+ Author      : Analia Cazal
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
